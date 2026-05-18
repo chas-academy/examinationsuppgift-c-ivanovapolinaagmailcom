2656ed5a223f1014f05df5e0bc137dca3c04c600
ivanovapolinaa@gmail.com <ivanovapolinaa@gmail.com>
2026-05-18 22:19:45 +0200

    Adding variables (students and scores)

--- main.c
+++ main.c
@@ -2,7 +2,18 @@
 #include <stdio.h>
 
 int main(){
+    char names[5][11];
+    float average [5];
 
+    int i;
+    int j;
+    int point;
+    int sum;
+
+    float total = 0;
+    float group_average;
+
+    int best = 0;
 
 
     return 0;
